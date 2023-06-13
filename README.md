3b6d7917f7bcbaa221be61d5120cb15a400b885f
Fiona Wu <fionawrm2003@gmail.com>
2023-06-13 00:43:57 -0700

    commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Simple EDA Generator Introduction
+### Author: Fiona Wu
+### UID: 905774219
+
+This is an introductionn to my streamlit application. This application is able to allow users to upload a csv file, provide dataset information, and generate apppropriate visualizations for different types of variables.
+
+Funcitonalities:
+
+ 1. Upload file by clicking "Browse files"
+ 2. Display dataset as data frame
+ 3. Display relevant dataset statistics
+ 4. View five number summary for numerical variables
+ 5. View proportions visualizations for categorical variables
+
+ 
--- app.py
+++ app.py
@@ -1,22 +1,70 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+st.title('Simple EDA Generator') # give warning, run python3 -m streamlit run app.py
 
-st.title('Title of your application') # give warning, run python3 -m streamlist run app.py
-st.markdown('this is **bold text**') # create markdown
-st.markdown('this is *italic text*')
+uploaded_file = st.file_uploader("Upload a file for EDA")
 
-st.sidebar.title('title of sidebar') # anything do in streamlit can do in sidebar, but need to specify
-st.sidebar.markdown('markdown **text** in the sidebar')
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write("File uploaded successfully!")
 
-agree = st.checkbox('I agree') # create checkbox
+    with st.expander("Dataset Information"):
+        checkbox1 = st.checkbox("Show Data Frame")
 
-if agree: # if click on checkbox, default is uncheck (don't need else)
-    st.write('Great!')  # operation that happens after clicked
-    st.markdown('this is markdown **text**')
+        if checkbox1:
+            st.write(df)
+        
+        checkbox2 = st.checkbox("Show Data Statistics")
+        if checkbox2:
+            num_rows = df.shape[0]
+            num_cols = df.shape[1]
 
-agree_sidebar = st.sidebar.checkbox('side bar checkbox')
+            # Count categorical, numerical, and boolean variables
+            categorical_vars = df.select_dtypes(include='object').columns.tolist()
+            numerical_vars = df.select_dtypes(include=['int', 'float']).columns.tolist()
+            bool_vars = df.select_dtypes(include='bool').columns.tolist()
 
-if agree_sidebar:
-    #st.write('side bar checked') # wrote on same page
-    st.sidebar.write('side bar checked') # wrote in sidebar
-    
+            # Display dataset relevant statistics
+            st.write(str(num_rows), " rows and ", str(num_cols), " columns")
+            st.write(str(len(categorical_vars)), " categorical variables")
+            st.write(str(len(numerical_vars)), " numerical variables")
+            st.write(str(len(bool_vars)), " boolean variables")
+
+    with st.expander("Select a Numerical Variable to Perform EDA"):
+        # select column
+        selected_column = st.selectbox("Select a column", df.columns)
+
+        if df[selected_column].dtype in ['int', 'float']:
+            # Display five-number summary
+            summary_stats = df[selected_column].describe().loc[['min', '25%', '50%', '75%', 'max']]
+            st.write("Five-Number Summary:")
+            st.write(summary_stats)
+
+            # Create distribution plot
+            fig, ax = plt.subplots(figsize=(10, 4))
+            sns.distplot(a=df[selected_column], hist=True, kde=False, rug=False)
+            ax.set_title(f"Distribution of {selected_column}")
+            ax.set_xlabel(selected_column)
+            ax.set_ylabel("Frequency")
+            st.pyplot(fig)
+        if df[selected_column].dtype == 'object':
+            # Calculate proportions
+            proportions = df[selected_column].value_counts(normalize=True)
+
+            # Display proportions in a table
+            st.write("Category Proportions:")
+            st.dataframe(proportions)
+
+            # Create customized barplot
+            fig, ax = plt.subplots(figsize=(10, 6))
+            sns.countplot(data=df, x=selected_column, ax=ax, palette='pastel')
+            ax.set_title(f"Barplot of {selected_column}")
+            ax.set_xlabel(selected_column)
+            ax.set_ylabel("Count")
+            ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
+            st.pyplot(fig)
+        else:
+            st.write("Warning: Selected Column Not Valid")
